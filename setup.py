8c7d8c7d388ecbb6634ded0f74281805e77fbd82
Remi Delbouys <remidbs@users.noreply.github.com>
2021-06-15 17:19:00 +0200

    Bump version: 0.0.4 → 0.0.5 (#7)

--- setup.py
+++ setup.py
@@ -81,6 +81,6 @@ setup(
     url="https://github.com/envinorma/alto",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.4",
+    version="0.0.5",
     zip_safe=False,
 )
